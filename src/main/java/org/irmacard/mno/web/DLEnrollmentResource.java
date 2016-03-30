022490f8020d733734c200ef7efd4975c5edb00f
Sietse Ringers <mail@sietseringers.net>
2016-03-30 17:00:45 +0200

    WIP on DL credential computing

--- src/main/java/org/irmacard/mno/web/DLEnrollmentResource.java
+++ src/main/java/org/irmacard/mno/web/DLEnrollmentResource.java
@@ -2,14 +2,20 @@ package org.irmacard.mno.web;
 
 import org.irmacard.credentials.info.CredentialIdentifier;
 import org.irmacard.credentials.info.InfoException;
+import org.irmacard.mno.common.DriverDemographicInfo;
 import org.irmacard.mno.common.EDLDataMessage;
 import org.irmacard.mno.common.EnrollmentStartMessage;
 import org.irmacard.mno.common.PassportVerificationResultMessage;
+import org.jmrtd.lds.MRZInfo;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
+@Path("v2/dl")
 public class DLEnrollmentResource extends GenericEnrollmentResource<EDLDataMessage> {
 	@GET
 	@Path("/start")
@@ -19,9 +25,8 @@ public class DLEnrollmentResource extends GenericEnrollmentResource<EDLDataMessa
 		return super.start();
 	}
 
-
 	@POST
-	@Path("/verify-dl")
+	@Path("/verify-document")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Override
@@ -33,6 +38,29 @@ public class DLEnrollmentResource extends GenericEnrollmentResource<EDLDataMessa
 	@Override
 	protected HashMap<CredentialIdentifier, HashMap<String, String>> getCredentialList(EnrollmentSession session)
 	throws InfoException { // TODO
+		HashMap<CredentialIdentifier, HashMap<String, String>> credentials = new HashMap<>();
+		EDLDataMessage data = session.getEDLDataMessage();
+		DriverDemographicInfo driver = data.getDriverDemographicInfo();
+
+		SimpleDateFormat bacDateFormat = new SimpleDateFormat("yyMMdd");
+		SimpleDateFormat hrDateFormat = new SimpleDateFormat("MMM d, y"); // Matches Android's default date format
+		Date dob;
+		Date expiry;
+
+		try {
+			dob = bacDateFormat.parse(driver.getDob());
+			expiry = bacDateFormat.parse(driver.getDoe());
+		}  catch (ParseException e) {
+			e.printStackTrace();
+			throw new InfoException("Failed to parse dates", e);
+		}
+
+		int[] lowAges = {12,16,18,21};
+		credentials.put(new CredentialIdentifier(SCHEME_MANAGER, ISSUER, "ageLower"), ageAttributes(lowAges, dob));
+
+		int[] highAges = {50, 60, 65, 75};
+		credentials.put(new CredentialIdentifier(SCHEME_MANAGER, ISSUER, "ageHigher"), ageAttributes(highAges, dob));
+
 		return null;
 	}
 }
