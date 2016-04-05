47e413acbae1973d8f1bc13e4f632ce8b094cb70
Fabian <f.vandenbroek@cs.ru.nl>
2016-04-05 14:50:03 +0200

    edl enrolment

--- src/main/java/org/irmacard/mno/web/DLEnrollmentResource.java
+++ src/main/java/org/irmacard/mno/web/DLEnrollmentResource.java
@@ -42,14 +42,14 @@ public class DLEnrollmentResource extends GenericEnrollmentResource<EDLDataMessa
 		EDLDataMessage data = session.getEDLDataMessage();
 		DriverDemographicInfo driver = data.getDriverDemographicInfo();
 
-		SimpleDateFormat bacDateFormat = new SimpleDateFormat("yyMMdd");
+		SimpleDateFormat eDLDateFormat = new SimpleDateFormat("ddMMyyyy");
 		SimpleDateFormat hrDateFormat = new SimpleDateFormat("MMM d, y"); // Matches Android's default date format
 		Date dob;
 		Date expiry;
 
 		try {
-			dob = bacDateFormat.parse(driver.getDob());
-			expiry = bacDateFormat.parse(driver.getDoe());
+			dob = eDLDateFormat.parse(driver.getDob());
+			expiry = eDLDateFormat.parse(driver.getDoe());
 		}  catch (ParseException e) {
 			e.printStackTrace();
 			throw new InfoException("Failed to parse dates", e);
@@ -61,6 +61,27 @@ public class DLEnrollmentResource extends GenericEnrollmentResource<EDLDataMessa
 		int[] highAges = {50, 60, 65, 75};
 		credentials.put(new CredentialIdentifier(SCHEME_MANAGER, ISSUER, "ageHigher"), ageAttributes(highAges, dob));
 
-		return null;
+		HashMap<String,String> nameAttributes = new HashMap<>();
+		String[] nameParts = splitFamilyName(driver.getFamilyName());
+		String firstnames = toTitleCase(driver.getGivenNames());
+		// The first of the first names is not always the person's usual name ("roepnaam"). In fact, the person's
+		// usual name need not even be in his list of first names at all. But given only the MRZ, there is no way of
+		// knowing what is his/her usual name... So we can only guess.
+		String firstname = toTitleCase(firstnames.split(" ")[0]);
+
+		nameAttributes.put("familyname", toTitleCase(nameParts[1]));
+		nameAttributes.put("prefix", nameParts[0]);
+		nameAttributes.put("firstnames", firstnames);
+		nameAttributes.put("firstname", firstname);
+
+		credentials.put(new CredentialIdentifier(SCHEME_MANAGER, ISSUER, "fullName"), nameAttributes);
+
+		HashMap<String, String> idDocumentAttributes = new HashMap<String, String>();
+		idDocumentAttributes.put("number", data.getDocumentNr());
+		idDocumentAttributes.put("expires", hrDateFormat.format(expiry));
+		idDocumentAttributes.put("nationality", driver.getCountry());
+		idDocumentAttributes.put("type", "Electronic Driving License");
+		credentials.put(new CredentialIdentifier(SCHEME_MANAGER, ISSUER, "idDocument"), idDocumentAttributes);
+		return credentials;
 	}
 }
